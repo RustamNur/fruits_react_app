4d6976df5250453b08596ce77fd18a27232d1809
RustamNur <136016020+RustamNur@users.noreply.github.com>
2023-08-18 12:27:49 +0300

    fruit_app

---
+++ header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="header">
+            <h1> Fruits</h1>
+            <h4>Fruits which you must taste </h4>
+      </header>
+    );
+  }
+}
+
+export default Header;
